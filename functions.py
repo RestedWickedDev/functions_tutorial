5619b8af0ebfa66d404f553c2b2ea13692f3ce70
RestedWickedDev <jesusdperez@gmail.com>
2020-09-03 19:44:11 -0700

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,32 @@
+def area(width, height):
+    result = width * height
+    return result
+result = area(5, 6)
+print(result)
+result = area(7,3)
+print(result)
+result = area(13, 13)
+print(result)    
+
+def subtract(num1, num2):
+    result = num1 - num2
+    return result
+
+result = subtract(653, 543)
+print(result)
+result = subtract(132, 54)
+print(result)
+
+def divide(dnum1, dnum2):
+    result = dnum1 / dnum2
+    return result
+result = divide(60, 3)
+print(result)
+
+def add(anum1, anum2):
+    result = anum1 + anum2
+    return result
+result = add(5, 24)
+print(result)
+result = add(64, 54)
+print(result)
